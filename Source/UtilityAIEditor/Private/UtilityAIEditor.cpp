30ac8625774ee6d33ca573841351798bd9cf06ed
MajorTomAW <tom.wrnr.private@gmail.com>
2025-05-19 16:54:09 +0200

    added an empty editor module

---
+++ Source/UtilityAIEditor/Private/UtilityAIEditor.cpp
@@ -0,0 +1,17 @@
+﻿#include "UtilityAIEditor.h"
+
+#define LOCTEXT_NAMESPACE "FUtilityAIEditorModule"
+
+void FUtilityAIEditorModule::StartupModule()
+{
+    
+}
+
+void FUtilityAIEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FUtilityAIEditorModule, UtilityAIEditor)
---
+++ Source/UtilityAIEditor/UtilityAIEditor.Build.cs
@@ -0,0 +1,22 @@
+﻿using UnrealBuildTool;
+
+public class UtilityAIEditor : ModuleRules
+{
+    public UtilityAIEditor(ReadOnlyTargetRules target) : base(target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new []
+        { 
+            "Core",
+        });
+
+        PrivateDependencyModuleNames.AddRange(new []
+        { 
+            "CoreUObject", 
+            "Engine", 
+            "Slate", 
+            "SlateCore"
+        });
+    }
+}
